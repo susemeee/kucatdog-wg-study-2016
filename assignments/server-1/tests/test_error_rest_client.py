705621f83cd2869a7e70148334ea6688022bad07
susemine* <susemeee@gmail.com>
2016-08-03 02:31:26 +0900

    올바르지 않은 데이터에 대한 422 코드 테스트 케이스 작성

--- assignments/server-1/tests/test_error_rest_client.py
+++ assignments/server-1/tests/test_error_rest_client.py
@@ -35,3 +35,40 @@ def test_cookie_client_put_malformed_data_returns_400(request_put):
 def test_cookie_client_delete_malformed_data_returns_400(request_delete):
     response = request_delete('/cookie/999', data='{')
     assert response.status_code == 400
+
+
+INVALID_FLAVOR_DATA = {'flavor': 'flowey', 'size': 15}
+INVALID_SIZE_DATA = {'flavor': 'zombie', 'size': 999}
+
+
+def test_cookie_client_post_invalid_flavor_data_returns_422(request_post):
+    invalid_data = INVALID_FLAVOR_DATA
+    response = request_post('/cookie/', data=invalid_data)
+    assert response.status_code == 422
+
+
+def test_cookie_client_post_invalid_size_data_returns_422(request_post):
+    invalid_data = INVALID_SIZE_DATA
+    response = request_post('/cookie/', data=invalid_data)
+    assert response.status_code == 422
+
+
+def _post_cookie(requet_post):
+    data = {'flavor': 'zombie', 'size': 20}
+    response = request_post('/cookie/', data=data)
+
+    return response.json()['id'], data
+
+
+def test_cookie_client_put_invalid_flavor_data_returns_422(request_post, request_put):
+    cookie_id, _ = _post_cookie(request_post)
+    invalid_data = INVALID_FLAVOR_DATA
+    response = request_put('/cookie/{}'.format(cookie_id), data=invalid_data)
+    assert response.status_code == 422
+
+
+def test_cookie_client_put_invalid_size_data_returns_422(request_post, request_put):
+    cookie_id, _ = _post_cookie(request_post)
+    invalid_data = INVALID_SIZE_DATA
+    response = request_put('/cookie/{}'.format(cookie_id), data=invalid_data)
+    assert response.status_code == 422

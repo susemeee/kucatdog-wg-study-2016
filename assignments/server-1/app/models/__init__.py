7b44c4614ced5267f3b45dcac5f425fefe4301d0
susemine* <susemeee@gmail.com>
2016-08-03 02:33:52 +0900

    DictModel에 대한 설명 추가

--- assignments/server-1/app/models/__init__.py
+++ assignments/server-1/app/models/__init__.py
@@ -3,6 +3,11 @@
 
 
 class DictModel(dict):
+    """
+    rollback() 함수가 있는 파이썬 dict입니다.
+    rollback 함수는 매 테스트의 종료마다 호출되며,
+    상호 테스트간 간섭이 없도록 데이터베이스를 초기화해주는 역할을 합니다.
+    """
     def rollback(self):
         self.clear()
 
